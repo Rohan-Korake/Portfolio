d5816f3b5c7e3345afd0a9aa3a17fcdb4ea51df8
Rohan Korake <rohannkorake@gmail.com>
2026-05-01 13:00:11 +0530

    add hamburger logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { handleNavigation } from "./navigation.js";
+import { toggleTheme } from "./theme.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   handleNavigation();
+  toggleTheme();
 });
--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,9 @@
 export function handleNavigation() {
   const hamBurger = document.getElementById("hamBurger");
-
+  const blurPage = document.getElementById("blurPage");
   const navMenu = document.getElementById("navMenu");
+
+  // handle hamburger click
   hamBurger.addEventListener("click", () => {
     if (navMenu.style.display == "flex") {
       hideMenu();
@@ -9,17 +11,56 @@ export function handleNavigation() {
       showMenu();
     }
   });
+
+  // handle empty click
+  blurPage.addEventListener("click", () => {
+    if (navMenu.style.display == "flex") {
+      hideMenu();
+    } else {
+      showMenu();
+    }
+  });
+
+  // handle navigation click
+  navMenu.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "homeSection":
+        break;
+
+      case "aboutSection":
+        break;
+
+      case "skillsSection":
+        break;
+
+      case "projectSection":
+        break;
+
+      case "contactSection":
+        break;
+
+      default:
+        hideMenu();
+        break;
+    }
+  });
 }
+
 const navMenu = document.getElementById("navMenu");
+const blurPage = document.getElementById("blurPage");
 
 // hide the nav menu option
 function hideMenu() {
+  blurPage.style.animation = "hide 1 0.5s ease";
+  blurPage.style.display = "none";
   navMenu.style.animation = "hide 1 0.5s ease";
   navMenu.style.display = "none";
 }
 
 // show the nav menu option
 function showMenu() {
+  blurPage.style.animation = "show 1 0.5s ease";
+  blurPage.style.display = "flex";
   navMenu.style.animation = "show 1 0.5s ease";
   navMenu.style.display = "flex";
 }
